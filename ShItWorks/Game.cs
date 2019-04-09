a0cce2674e4df6f56eaa14e0006c26cb3f07931f
DANKPC420\rudol <rudolfsagris@gmail.com>
2019-04-09 16:50:01 +0300

    basic stuff

---
+++ ShItWorks/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace ShItWorks
+{
+    public sealed class Game : GameWindow
+    {
+        public static Game Current;
+
+        public Game() : base(640, 480)
+        {
+            Title = "ShItWorks Game";
+            Current = this;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.LightSkyBlue);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            SwapBuffers();
+            
+        }
+    }
+}
--- ShItWorks/Program.cs
+++ ShItWorks/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
 
 namespace ShItWorks
 {
     class Program
     {
+        public static Game Game = new Game();
+
         static void Main(string[] args)
         {
+            Game.Current.Run(60.0);
         }
     }
 }
