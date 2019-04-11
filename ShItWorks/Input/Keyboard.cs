e089cac45101535cb621c40ae8054e935796848f
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2019-04-11 12:08:06 +0300

    shitworks.input.keyboard extensions

---
+++ ShItWorks/Input/Keyboard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenTK;
+
+namespace ShItWorks.Input
+{
+    public static class Keyboard
+    {
+        private static OpenTK.Input.KeyboardState lastState;
+        private static OpenTK.Input.KeyboardState currentState;
+        private static bool stateSet = false;
+        private static uint stateFrame = 0;
+        public static OpenTK.Input.KeyboardState CurrentState
+        { get
+            {
+                if(stateSet == false || stateFrame != Game.Current.TotalFrames)
+                {
+                    lastState = currentState;
+                    currentState = OpenTK.Input.Keyboard.GetState();
+                    stateSet = true;
+                    stateFrame = Game.Current.TotalFrames;
+                }
+                return currentState;
+            }
+        }
+
+        public static bool KeyDown(OpenTK.Input.Key key)
+        {
+            return currentState.IsKeyDown(key);
+        }
+
+        public static bool KeyPressed(OpenTK.Input.Key key)
+        {
+            return (currentState.IsKeyDown(key) && lastState.IsKeyUp(key));
+        }
+
+        public static bool KeyUp(OpenTK.Input.Key key)
+        {
+            return currentState.IsKeyUp(key);
+        }
+
+        public static bool KeyReleased(OpenTK.Input.Key key)
+        {
+            return (currentState.IsKeyUp(key) && lastState.IsKeyDown(key));
+        }
+    }
+}
---
+++ ShItWorks/Nodes/Scripted/RotatingCubeManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ShItWorks.Nodes;
+using ShItWorks.Input;
+using ShItWorks.Logic;
+
+namespace ShItWorks.Nodes.Scripted
+{
+    class RotatingCubeManager : BaseNode, ILoopLogic
+    {
+        public List<BaseNode> allCubes = new List<BaseNode>();
+
+        public RotatingCubeManager()
+        {
+            Dispatcher.AddLoopDispatcher(this);
+        }
+
+        public void OnLoop()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Dispatcher.RemoveLoopDispatcher(this);
+            base.Dispose(disposing);
+        }
+    }
+}
